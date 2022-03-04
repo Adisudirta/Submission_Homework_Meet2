b60ba537f61ae7bb396d52cdd5157957bb62b35c
Adisudirta <yanadisudirta@gmail.com>
2022-03-04 13:57:21 +0800

    Successfully completed question 1

---
+++ question1.sql
@@ -0,0 +1,14 @@
+--QUESTION 1
+--:List all Category Names ordered alphabetically.
+
+SELECT CategoryName FROM Category ORDER BY CategoryName ASC;
+
+--Output:
+-- Beverages
+-- Condiments
+-- Confections
+-- Dairy Products
+-- Grains/Cereals
+-- Meat/Poultry
+-- Produce
+-- Seafood
